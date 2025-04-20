895a3c1135d38d3ac28bda0d7e2e449f73eb6904
Diego Ramirez <dr01191115@gmail.com>
2025-04-19 20:41:45 -0600

    Add more structure to the main code

--- main.py
+++ main.py
@@ -11,4 +11,20 @@ class Main:
 
     def __init__(self):
         pyxel.init(128, 128, title=self.title_status)
+        # resource for characters, sprite sounds and mobs
+        pyxel.load("resource-characters.pyxres", excl_musics=True, excl_tilemaps=True)
+        # world resource and ambient music
+        # pyxel.load("resource-world.pyxres", excl_images=True, excl_sounds=True)
+        # set up variables
+        self.setup()
+        pyxel.run(self.update, self.draw)
+
+    def setup(self):
+        "Set up variables for initial loading and further reloads."
+
+    def update(self):
+        "Main update"
+
+    def draw(self):
+        "main draw"
         
